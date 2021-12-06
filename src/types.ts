3cb93e8f68a02de0c915d8f192c754ea58aad68c
jxguo <guojiaxuan@eventslack.com>
2021-12-06 13:36:08 +0800

    refactor: move response data types into types.ts

--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,67 @@ export interface Contents {
   content: string
   content_rendered: string
 }
+
+export interface SiteInfo {
+  title: string
+  slogan: string
+  description: string
+  domain: string
+}
+
+export interface SiteStats {
+  topic_max: number
+  member_max: number
+}
+
+export interface SiteNode extends Avatars {
+  id: number
+  name: string
+  url: string
+  title: string
+  title_alternative: string
+  topics: number
+  header: string
+  footer: string
+  created: number
+  stars: number
+  aliases: string[]
+  root: boolean
+  parent_node_name: string
+}
+
+export interface Topic extends Contents {
+  id: number
+  title: string
+  url: string
+  replies: number
+  member: Member
+  node: SiteNode
+  created: number
+  deleted: number
+  last_modified: number
+  last_touched: number
+  last_reply_by: string
+}
+
+export interface Member extends Avatars {
+  id: number
+  username: string
+  tagline: string
+  url: string
+  github: string
+  psn: string
+  website: string
+  twitter: string
+  location: string
+  bio: string
+  created: number
+}
+
+export interface Reply extends Contents {
+  id: number
+  thanks?: number
+  member: Member
+  created: number
+  last_modified: number
+}
--- src/v1.ts
+++ src/v1.ts
@@ -1,40 +1,15 @@
-import { Avatars, Contents, PrivateClient } from './types'
+import { Member, PrivateClient, SiteInfo, SiteNode, SiteStats, Topic } from './types'
 
-export interface SiteInfo {
-  title: string
-  slogan: string
-  description: string
-  domain: string
-}
 export async function getSiteInfo(this: PrivateClient) {
   const res = await this._client.get<SiteInfo>('/site/info.json')
   return res.data
 }
 
-export interface SiteStats {
-  topic_max: number
-  member_max: number
-}
 export async function getSiteStats(this: PrivateClient) {
   const res = await this._client.get<SiteStats>('/site/stats.json')
   return res.data
 }
 
-export interface SiteNode extends Avatars {
-  id : number
-  name : string
-  url : string
-  title : string
-  title_alternative : string
-  topics : number
-  header : string
-  footer : string
-  created : number
-  stars: number
-  aliases: string[]
-  root: boolean
-  parent_node_name: string
-}
 export async function getAllSiteNodes(this: PrivateClient) {
   const res = await this._client.get<SiteNode[]>('/nodes/all.json')
   return res.data
@@ -52,19 +27,6 @@ export async function getSiteNode(this: PrivateClient, idOrName: number | string
   return res.data
 }
 
-export interface Topic extends Contents {
-  id: number
-  title: string
-  url: string
-  replies: number
-  member: Member
-  node: SiteNode
-  created: number
-  deleted: number
-  last_modified: number
-  last_touched: number
-  last_reply_by: string
-}
 export async function getTopicsByType(this: PrivateClient, type: 'latest' | 'hot') {
   const res = await this._client.get<Topic[]>(`/topics/${type}.json`)
   return res.data
@@ -93,31 +55,11 @@ export async function getTopicsBySearch(this: PrivateClient, searchParams: Topic
   return res.data
 }
 
-export interface Member extends Avatars {
-  id : number
-  username : string
-  tagline : string
-  url : string
-  github: string
-  psn: string
-  website : string
-  twitter : string
-  location : string
-  bio : string
-  created : number
-}
 export async function getMember(this: PrivateClient, username: string) {
   const res = await this._client.get<Member>('/members/show.json', { params: { username } })
   return res.data
 }
 
-export interface Reply extends Contents {
-  id: number
-  thanks?: number
-  member: Member
-  created: number
-  last_modified: number
-}
 export async function getReplies(this: PrivateClient, topic_id: number, page: number = 1, page_size: number = 10) {
   const res = await this._client.get<Member>('/replies/show.json', { params: { topic_id, page, page_size } })
   return res.data
